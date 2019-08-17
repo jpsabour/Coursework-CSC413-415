cb8c6a1319ee4f18f7527846a38a3b9c7428ccd6
jpsabour <jpsabour@gmail.com>
2019-08-17 16:44:59 -0700

    Add files via upload

---
+++ csc415-p2-jpsabour/cmake-build-debug/CMakeFiles/p2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p2.dir/main.c.o"
+  "p2.pdb"
+  "p2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
